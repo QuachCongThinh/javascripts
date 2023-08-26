c70b27eda06937cb441d3def0f5ccaae4f1f072e
QuachCongThinh <qcthinh3105@gmail.com>
2023-08-26 15:05:30 +0700

    update main

--- main.js
+++ main.js
@@ -1,16 +1,103 @@
-Array.prototype.forEach2 = function(callback){ 
+Array.prototype.filter2 = function(callback){
+  var output = [];
   for(var index in this){
-    console.log('index:', index)
+    if(this.hasOwnProperty(index)){
+      var results = callback(this[index], index, this)
+      if (results){
+        output.push(this[index]);
+      }
+    }
   }
+  return output;
 }
 
-var courses = [
-  'Javascript',
-  'HTML,CSS'
+const juices = [
+  {
+    id: 1,
+    name: 'Oi',
+    coin: 100
+  },
+  {
+    id: 2,
+    name: 'Man',
+    coin: 780
+  },
+  {
+    id: 3,
+    name: 'Thanh Long',
+    coin: 500
+  },
+  {
+    id: 4,
+    name: 'Vai',
+    coin: 630
+  },
 ]
 
-console.log(courses)
-
-courses.forEach2(function(course,index){
-  console.log(courses[index])
+var getJuice = juices.filter2(function(juice){
+  console.log(juice);
+  return juices.coin >= 600;
 })
+//  
+
+
+
+
+
+
+// Datatypes
+/**  Primtive types
+  * string
+  * number
+  * boolean
+  * null
+  * undefined
+  * symbol
+*/
+/**  reference types
+  * object: types in object
+  * array: Array., for, find, map, reduce, filter, ... function in array
+  * date
+  * ...
+*/
+
+// Functions
+/**
+ * Declaration Function
+ * Expression Function
+ * Arrow Function
+ * IIFE function
+ */
+
+// build-in function in js
+
+/**
+ * loop
+ * switch-case
+ * conditions
+ * break/continue
+ */
+
+
+/** Advanced
+ * Map
+ * Hoisting
+ * Scope
+ * comparisons
+ * asynchronous: async-await, promise, try-catch 
+ * callback, fallback
+ * stack, event loop, callback queue
+ * Event listeners
+ * closure
+ * api: get, post, patch
+ * hosting, domain, http, ssl
+ * es version
+ * module, common, udm
+ * ajax
+ * this
+ * binding
+ * class
+ * spread, rest
+ */
+
+// => master
